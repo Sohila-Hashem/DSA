10aac2eb04b5cfebe074e61f25ef9c64f362d1fb
Storm <sohila.samir1911@gmail.com>
2024-04-08 20:41:38 +0200

    refactoring in linkedList.js & bugs fixes

--- DS/linked-list.js
+++ DS/linked-list.js
@@ -5,56 +5,88 @@ class Node {
     }
 }
 
-class LinkedList {
+export class LinkedList {
+    #head;
+    #tail;
+    #size;
+
     constructor(head = null) {
-        this.head = head;
-        this.tail = null;
-        this.size = 0;
+        this.#head = head;
+        this.#tail = null;
+        this.#size = 0;
+    }
+
+    // returns a deep copy of the Head
+    getHead() {
+        return Object.assign({}, this.#head);
+    }
+
+    getTail() {
+        return Object.assign({}, this.#tail);
+    }
+
+    get size() {
+        return [this.#size][0];
     }
 
     // insert to the first
     insertHead(data) {
-        const node = new Node(data, this.head);
+        if (data === undefined)
+            throw new Error("argument data must be defined");
 
-        if (!this.tail) {
-            this.tail = node;
+        const node = new Node(data, this.#head);
+
+        this.#head = node;
+
+        if (!this.#tail) {
+            this.#tail = node;
         }
 
-        this.size++;
+        this.#size++;
     }
 
     // insert to the end
     insert(data) {
+        if (data === undefined) throw new Error("argument must be defined");
+
         let node = new Node(data);
 
-        if (!this.tail && !this.head) {
-            this.head = node;
+        if (!this.#tail && !this.#head) {
+            this.#head = node;
         } else {
-            this.tail.next = node;
+            this.#tail.next = node;
         }
 
-        this.tail = node;
-        this.size++;
+        this.#tail = node;
+        this.#size++;
         return;
     }
 
     // insert at a giving index
-    insertAt(index, data) {
-        let currentIndex = 1;
-        let current = this.head;
+    insertAt(position, data) {
+        if (data === undefined || position === undefined)
+            throw new Error("position and data arguments must be defined");
 
-        if (index > this.size || index < 1) {
-            console.log("Index is out of range");
-            return;
-        } else if (index === 1) {
-            this.insertHead(data);
+        let currentIndex = 1;
+        let current = this.#head;
+
+        if (position > this.#size || position < 1) {
+            throw new Error("Position is out of range");
+        }
+
+        if (position === 1) {
+            return this.insertHead(data);
+        }
+
+        if (position === this.#size) {
+            this.insert(data);
             return;
         }
 
         while (current) {
-            if (currentIndex === index) {
+            if (currentIndex === position - 1) {
                 current.next = new Node(data, current.next);
-                this.size++;
+                this.#size++;
                 return;
             } else {
                 current = current.next;
@@ -66,33 +98,47 @@ class LinkedList {
 
     // remove first
     removeHead() {
-        this.head = this.head.next;
-        this.size--;
+        this.#head = this.#head.next;
+        this.#size--;
     }
 
     // remove at the end
-    // { data: "012", next: { data: "234", next: null }}
-    removeTail() {
-        let current = this.head;
+    remove() {
+        let current = this.#head;
 
         while (current) {
             if (current.next.next === null) {
-                this.tail = current;
+                this.#tail = current;
                 current.next = null;
-                this.size--;
+                this.#size--;
+                break;
             }
             current = current.next;
         }
         return;
     }
 
-    // remove at a given index
-    removeAt(index) {
+    // remove at a given position
+    removeAt(position) {
+        if (position === undefined) throw new Error("position must be defined");
+
         let currentIndex = 1;
-        let current = this.head;
+        let current = this.#head;
+
+        if (position < 1 || position > this.#size) {
+            throw new Error("Position out of range");
+        }
+
+        if (position === 1) {
+            return this.removeHead();
+        }
+
+        if (position === this.#size) {
+            return this.remove();
+        }
 
         while (current) {
-            if (currentIndex === index) {
+            if (currentIndex === position) {
                 current.next = current.next.next;
                 return;
             }
@@ -104,28 +150,32 @@ class LinkedList {
 
     // clear the list
     clearList() {
-        this.head = null;
-        this.size = 0;
+        this.#head = null;
+        this.#tail = null;
+        this.#size = 0;
     }
 
     // return and print a specific node
-    getAt(index) {
-        let currentIndex = 0;
-        let current = this.head;
+    getAt(position) {
+        if (position === undefined) throw new Error("position must be defined");
 
-        if (index < 1 || index > this.size) {
-            console.log("Index out of range");
-            return;
-        } else if (index === 1) {
-            console.log(this.head.data);
-            return this.head.data;
-        } else if (index === this.size) {
-            console.log(this.tail.data);
-            return this.tail.data;
+        let currentIndex = 1;
+        let current = this.#head;
+
+        if (position < 1 || position > this.#size) {
+            throw new Error("Position out of range");
+        }
+
+        if (position === 1) {
+            return this.getHead();
+        }
+
+        if (position === this.#size) {
+            return this.getTail();
         }
 
         while (current) {
-            if (currentIndex === index) {
+            if (currentIndex === position - 1) {
                 console.log(current.data);
                 return current.data;
             }
@@ -136,7 +186,7 @@ class LinkedList {
 
     // print the list data
     printList() {
-        let current = this.head;
+        let current = this.#head;
 
         while (current) {
             console.log(current.data);
@@ -147,7 +197,7 @@ class LinkedList {
     // returns the linked list into an array
     toArray() {
         const llArray = [];
-        let current = this.head;
+        let current = this.#head;
 
         while (current) {
             llArray.push(current);
@@ -157,3 +207,30 @@ class LinkedList {
         return llArray;
     }
 }
+
+// const ll = new LinkedList();
+
+// ll.insert("Sohila");
+// ll.insert("Ahmed");
+// ll.insert("Mohamed");
+// ll.insert("A");
+
+// ll.insertHead("Samir");
+
+// ll.insertAt(ll.size, "Amra");
+// ll.insertAt(2, "Waffle");
+// ll.insertAt(1, "Sef");
+
+// ll.removeHead();
+
+// ll.remove();
+
+// ll.removeAt(3);
+
+// console.log(ll.getTail());
+
+// console.log(ll.getHead());
+
+// console.log(ll.getAt(1));
+
+// console.log(ll.size);
