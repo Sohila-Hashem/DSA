10fc2a30ac451f4cee2370eff089ce5389ebe33b
Storm <sohila.samir1911@gmail.com>
2024-04-21 18:23:55 +0200

    new - implementing stack using linked lists - StackLL

--- DS/stack.js
+++ DS/stack.js
@@ -1,3 +1,5 @@
+import { LinkedList } from "./linked-list.js";
+
 class Stack {
     #stack;
     #size;
@@ -84,3 +86,83 @@ class Stack {
 // stack.push(5);
 // stack.push(5);
 // stack.push(5);
+
+class StackLL {
+    #stack;
+
+    constructor() {
+        this.#stack = new LinkedList();
+    }
+
+    get size() {
+        return this.#stack.size;
+    }
+
+    getStack() {
+        const linkedListData = [];
+        let current = this.#stack.head;
+
+        while (current) {
+            linkedListData.push(current.data);
+            current = current.next;
+        }
+
+        return linkedListData;
+    }
+
+    push(item) {
+        if (!item) throw new Error("Expecting an argument of type any");
+
+        this.#stack.insert(item);
+    }
+
+    pop() {
+        if (!this.#stack.size) throw new Error("Stack Underflow");
+
+        return this.#stack.remove().data;
+    }
+
+    peek() {
+        if (!this.#stack.size) throw new Error("Stack Underflow");
+
+        return this.#stack.tail.data;
+    }
+
+    isEmpty() {
+        return !!!this.#stack.size;
+    }
+}
+
+// const stackL = new StackLL();
+
+// is empty?
+// console.log(stackL.isEmpty());
+
+// checking an underflow error
+// stackL.pop();
+
+// checking an underflow error for
+// getting a peek value
+// stackL.peek();
+
+// push
+// stackL.push(1);
+// stackL.push(2);
+// stackL.push(3);
+// stackL.push(4);
+// stackL.push(5);
+
+// is empty?
+// console.log(stackL.isEmpty());
+
+// pop
+// console.log(stackL.pop());
+
+// peak
+// console.log(stackL.peek());
+
+// get stackL
+// console.log(stackL.getStack());
+
+// size
+// console.log(stackL.size);
