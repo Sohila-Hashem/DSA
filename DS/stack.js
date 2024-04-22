092dda0d6c95530f1ca8879083a3e5da0b3e5371
Storm <sohila.samir1911@gmail.com>
2024-04-22 20:46:06 +0200

    new - launching tests for the stack (linked-list implementation) - StackLL/test

--- DS/stack.js
+++ DS/stack.js
@@ -1,125 +1,91 @@
 import { LinkedList } from "./linked-list.js";
 
 export class Stack {
-    #stack;
-    #size;
+	#stack;
+	#size;
 
-    constructor(size) {
-        this.#size = size || 10;
-        this.#stack = [];
-    }
+	constructor(size) {
+		this.#size = size || 10;
+		this.#stack = [];
+	}
 
-    // returning a deep copy of the stack
-    // to prevent any accidental mutation
-    getStack() {
-        return this.#stack;
-    }
+	// returning a deep copy of the stack
+	// to prevent any accidental mutation
+	getStack() {
+		return this.#stack;
+	}
 
-    get size() {
-        return this.getStack().length;
-    }
+	get size() {
+		return this.getStack().length;
+	}
 
-    push(item) {
-        if (!item) throw new Error("Expecting an argument of type any");
+	push(item) {
+		if (!item) throw new Error("Expecting an argument of type any");
 
-        if (this.#stack.length >= this.#size) throw new Error("Stack Overflow");
+		if (this.#stack.length >= this.#size) throw new Error("Stack Overflow");
 
-        this.#stack.push(item);
-    }
+		this.#stack.push(item);
+	}
 
-    pop() {
-        if (!this.#stack.length) throw new Error("Stack Underflow");
+	pop() {
+		if (!this.#stack.length) throw new Error("Stack Underflow");
 
-        return this.#stack.pop();
-    }
+		return this.#stack.pop();
+	}
 
-    peek() {
-        if (!this.#stack.length) throw new Error("Stack is Empty");
+	peek() {
+		if (!this.#stack.length) throw new Error("Stack is Empty");
 
-        return this.#stack[this.#stack.length - 1];
-    }
+		return this.#stack[this.#stack.length - 1];
+	}
 
-    isEmpty() {
-        return !!!this.#stack.length;
-    }
+	isEmpty() {
+		return !!!this.#stack.length;
+	}
 }
 
 export class StackLL {
-    #stack;
+	#stack;
 
-    constructor() {
-        this.#stack = new LinkedList();
-    }
+	constructor() {
+		this.#stack = new LinkedList();
+	}
 
-    get size() {
-        return this.#stack.size;
-    }
+	get size() {
+		return this.#stack.size;
+	}
 
-    getStack() {
-        const linkedListData = [];
-        let current = this.#stack.head;
+	getStack() {
+		const linkedListData = [];
+		let current = this.#stack.head;
 
-        while (current) {
-            linkedListData.push(current.data);
-            current = current.next;
-        }
+		while (current) {
+			linkedListData.push(current.data);
+			current = current.next;
+		}
 
-        return linkedListData;
-    }
+		return linkedListData;
+	}
 
-    push(item) {
-        if (!item) throw new Error("Expecting an argument of type any");
+	push(item) {
+		if (!item) throw new Error("Expecting an argument of type any");
 
-        this.#stack.insert(item);
-    }
+		this.#stack.insert(item);
+	}
 
-    pop() {
-        if (!this.#stack.size) throw new Error("Stack Underflow");
+	pop() {
+		if (!this.#stack.size) throw new Error("Stack Underflow");
 
-        return this.#stack.remove().data;
-    }
+		return this.#stack.remove().data;
+	}
 
-    peek() {
-        if (!this.#stack.size) throw new Error("Stack Underflow");
+	peek() {
+		if (!this.#stack.size) throw new Error("Stack Underflow");
 
-        return this.#stack.tail.data;
-    }
+		return this.#stack.tail.data;
+	}
 
-    isEmpty() {
-        return !!!this.#stack.size;
-    }
+	isEmpty() {
+		return !!!this.#stack.size;
+	}
 }
-
-// const stackL = new StackLL();
-
-// is empty?
-// console.log(stackL.isEmpty());
-
-// checking an underflow error
-// stackL.pop();
-
-// checking an underflow error for
-// getting a peek value
-// stackL.peek();
-
-// push
-// stackL.push(1);
-// stackL.push(2);
-// stackL.push(3);
-// stackL.push(4);
-// stackL.push(5);
-
-// is empty?
-// console.log(stackL.isEmpty());
-
-// pop
-// console.log(stackL.pop());
-
-// peak
-// console.log(stackL.peek());
-
-// get stackL
-// console.log(stackL.getStack());
-
-// size
-// console.log(stackL.size);
--- DS/test/stack.spec.js
+++ DS/test/stack.spec.js
@@ -2,115 +2,224 @@ import assert from "assert";
 import { Stack, StackLL } from "../stack.js";
 
 describe("Stack", function () {
-    describe("push", function () {
-        it("should mutate the stack with the element added", function () {
-            const stack = new Stack();
-            stack.push(1);
-            assert.strictEqual(stack.getStack()[0], 1);
-        });
+	describe("push", function () {
+		it("should mutate the stack with the element added", function () {
+			const stack = new Stack();
+			stack.push(1);
+			assert.strictEqual(stack.getStack()[0], 1);
+		});
 
-        it("should throw an Overflow error if the stack is full", function () {
-            const stack = new Stack(3);
-            stack.push(1);
-            stack.push(2);
-            stack.push(3);
+		it("should throw an Overflow error if the stack is full", function () {
+			const stack = new Stack(3);
 
-            assert.throws(
-                function () {
-                    stack.push(4);
-                },
-                {
-                    name: "Error",
-                    message: "Stack Overflow",
-                }
-            );
-        });
-    });
+			stack.push(1);
+			stack.push(2);
+			stack.push(3);
 
-    describe("getStack", function () {
-        it("should return the stack", function () {
-            const stack = new Stack();
-            stack.push(1);
-            assert.deepStrictEqual(stack.getStack(), [1]);
-        });
-    });
+			assert.throws(
+				function () {
+					stack.push(4);
+				},
+				{
+					name: "Error",
+					message: "Stack Overflow",
+				}
+			);
+		});
+	});
 
-    describe("pop", function () {
-        it("should return the last stack element", function () {
-            const stack = new Stack();
-            stack.push(1);
-            assert.strictEqual(stack.pop(), 1);
-        });
+	describe("getStack", function () {
+		it("should return the stack", function () {
+			const stack = new Stack();
+			stack.push(1);
+			assert.deepStrictEqual(stack.getStack(), [1]);
+		});
+	});
 
-        it("should throw an error if the stack is empty", function () {
-            const stack = new Stack();
-            assert.throws(
-                function () {
-                    stack.pop();
-                },
-                {
-                    name: "Error",
-                    message: "Stack Underflow",
-                }
-            );
-        });
-    });
+	describe("pop", function () {
+		it("should return the last stack element", function () {
+			const stack = new Stack();
+			stack.push(1);
+			assert.strictEqual(stack.pop(), 1);
+		});
 
-    describe("isEmpty", function () {
-        it("should return true if the stack is empty", function () {
-            const stack = new Stack();
-            assert.strictEqual(stack.isEmpty(), true);
-        });
+		it("should throw an error if the stack is empty", function () {
+			const stack = new Stack();
+			assert.throws(
+				function () {
+					stack.pop();
+				},
+				{
+					name: "Error",
+					message: "Stack Underflow",
+				}
+			);
+		});
+	});
 
-        it("should return false if the stack is not empty", function () {
-            const stack = new Stack();
-            stack.push(1);
-            assert.strictEqual(stack.isEmpty(), false);
-        });
-    });
+	describe("isEmpty", function () {
+		it("should return true if the stack is empty", function () {
+			const stack = new Stack();
+			assert.strictEqual(stack.isEmpty(), true);
+		});
 
-    describe("peek", function () {
-        it("should return the last element in the stack", function () {
-            const stack = new Stack();
-            stack.push(1);
-            stack.push(2);
-            assert.strictEqual(stack.peek(), 2);
-        });
+		it("should return false if the stack is not empty", function () {
+			const stack = new Stack();
+			stack.push(1);
+			assert.strictEqual(stack.isEmpty(), false);
+		});
+	});
 
-        it("should throw an error if the stack is empty", function () {
-            const stack = new Stack();
-            assert.throws(
-                function () {
-                    stack.peek();
-                },
-                {
-                    name: "Error",
-                    message: "Stack is Empty",
-                }
-            );
-        });
+	describe("peek", function () {
+		it("should return the last element in the stack", function () {
+			const stack = new Stack();
+			stack.push(1);
+			stack.push(2);
+			assert.strictEqual(stack.peek(), 2);
+		});
 
-        it("should not mutate the stack after invoking the method", function () {
-            const stack = new Stack();
-            stack.push(1);
-            stack.push(2);
+		it("should throw an error if the stack is empty", function () {
+			const stack = new Stack();
+			assert.throws(
+				function () {
+					stack.peek();
+				},
+				{
+					name: "Error",
+					message: "Stack is Empty",
+				}
+			);
+		});
 
-            const stackCopy1 = [...stack.getStack()];
+		it("should not mutate the stack after invoking the method", function () {
+			const stack = new Stack();
+			stack.push(1);
+			stack.push(2);
 
-            stack.peek();
+			const stackCopy1 = [...stack.getStack()];
 
-            const stackCopy2 = [...stack.getStack()];
+			stack.peek();
 
-            assert.deepStrictEqual(stackCopy1, stackCopy2);
-        });
-    });
+			const stackCopy2 = [...stack.getStack()];
 
-    describe("size property", function () {
-        it("should return the size of the stack", function () {
-            const stack = new Stack();
-            stack.push(1);
+			assert.deepStrictEqual(stackCopy1, stackCopy2);
+		});
+	});
 
-            assert.strictEqual(stack.size, 1);
-        });
-    });
+	describe("size property", function () {
+		it("should return the size of the stack", function () {
+			const stack = new Stack();
+			stack.push(1);
+
+			assert.strictEqual(stack.size, 1);
+		});
+	});
+});
+
+describe("StackLL", function () {
+	describe("push", function () {
+		it("should push a node to the stack without any error", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+
+			assert.doesNotThrow(function () {
+				stackLL.push(1);
+			});
+		});
+
+		it("should mutate the stack with the item added", function () {
+			const stackLL = new StackLL();
+			const stackBefore = stackLL.getStack();
+			stackLL.push(1);
+			const stackAfter = stackLL.getStack();
+
+			assert.strictEqual(
+				stackBefore[0] === undefined,
+				stackAfter[0] === 1
+			);
+		});
+
+		it("should not throw an error no matter how much pushed", function () {
+			const stackLL = new StackLL();
+			let n = 100;
+
+			for (let i = 1; i <= n; i++) {
+				stackLL.push(i);
+			}
+
+			assert.deepStrictEqual(stackLL.getStack().length, n);
+		});
+	});
+
+	describe("getStack", function () {
+		it("should not throw error", function () {
+			const stackLL = new StackLL();
+			assert.doesNotThrow(function () {
+				stackLL.getStack();
+			});
+		});
+
+		it("should not throw error", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+
+			assert.deepStrictEqual(stackLL.getStack(), [1]);
+		});
+	});
+
+	describe("pop", function () {
+		it("should return the last pushed element in the stack", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+			stackLL.push(2);
+
+			assert.strictEqual(stackLL.pop(), 2);
+		});
+
+		it("should mutate the stack (decreasing one node)", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+			stackLL.push(2);
+			const stackBefore = stackLL.getStack();
+			stackLL.pop();
+			const stackAfter = stackLL.getStack();
+
+			assert.strictEqual(
+				stackBefore[0] === 1 && stackBefore[1] === 2,
+				stackAfter[0] === 1 && !stackAfter[1]
+			);
+		});
+	});
+
+	describe("peek", function () {
+		it("should return the last pushed value", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+
+			assert.strictEqual(stackLL.peek(), 1);
+		});
+
+		it("should not mutate the stack after invoking the method", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+			const stackBefore = stackLL.getStack();
+			stackLL.peek();
+			const stackAfter = stackLL.getStack();
+
+			assert.deepStrictEqual(stackBefore, stackAfter);
+		});
+	});
+
+	describe("isEmpty", function () {
+		it("should return true if the stack is empty", function () {
+			const stackLL = new StackLL();
+			assert.strictEqual(stackLL.isEmpty(), true);
+		});
+
+		it("should return false if the stack is not empty", function () {
+			const stackLL = new StackLL();
+			stackLL.push(1);
+			assert.notStrictEqual(stackLL.isEmpty(), true);
+		});
+	});
 });
