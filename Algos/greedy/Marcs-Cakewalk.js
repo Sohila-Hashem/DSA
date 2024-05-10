3dbb0d872c2dae2c8665077e9f52d6e376396765
Storm <sohila.samir1911@gmail.com>
2024-05-10 17:19:37 +0300

    update - adding metadata about the problem - Marcs-Cakewalk.js/file

--- Algos/greedy/Marcs-Cakewalk.js
+++ Algos/greedy/Marcs-Cakewalk.js
@@ -1,10 +1,17 @@
-const solution = (calories) => {
+// link -> https://www.hackerrank.com/challenges/marcs-cakewalk/problem?isFullScreen=false
+
+/**
+ * @param calorie an array of integers containing the calorie counts for each cupcake
+ * @returns a Long Number Type representing the minimum miles necessary
+ */
+
+const solution = (calorie) => {
 	let min = 0;
 
-	calories.sort((a, b) => b - a);
+	calorie.sort((a, b) => b - a);
 
-	for (let i = 0; i < calories.length; i++) {
-		min = min + calories[i] * Math.pow(2, i);
+	for (let i = 0; i < calorie.length; i++) {
+		min = min + calorie[i] * Math.pow(2, i);
 	}
 
 	return min;
