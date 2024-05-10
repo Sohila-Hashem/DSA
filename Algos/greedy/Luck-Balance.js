411553ab4e6c7ea04c1ac43d10b00168ca4f6a22
Storm <sohila.samir1911@gmail.com>
2024-05-10 17:14:25 +0300

    new - adding solution to luck-balance  greedy algorithm problem from HackerRank

---
+++ Algos/greedy/Luck-Balance.js
@@ -0,0 +1,39 @@
+// link -> https://www.hackerrank.com/challenges/luck-balance/problem?isFullScreen=false
+
+/**
+ * @param K the number of important contests Lena can lose
+ * @param contests a 2D array of integers where each contests[i] contains two integers that represent the luck balance and importance of the ith contest
+ * @returns an Integer representing the maximum luck balance achievable
+ */
+const solution = (k, contests) => {
+	let maxLuck = 0;
+
+	contests.sort((a, b) => {
+		return b[0] - a[0];
+	});
+
+	for (let i = 0; i < contests.length; i++) {
+		if (contests[i][1]) {
+			if (k) {
+				maxLuck = maxLuck + contests[i][0];
+				k--;
+			} else {
+				maxLuck = maxLuck - contests[i][0];
+			}
+		} else {
+			maxLuck = maxLuck + contests[i][0];
+		}
+	}
+
+	return maxLuck;
+};
+
+const output = solution(3, [
+	[5, 1],
+	[2, 1],
+	[1, 1],
+	[8, 1],
+	[10, 0],
+	[5, 0],
+]);
+console.log(output);
